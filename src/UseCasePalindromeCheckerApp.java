0d2801bb132f3a808946a69541818b49466f1ed2
MalviSood <malvisood@gmail.com>
2026-02-25 09:37:27 +0530

    UC4:Character Array

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
 public class UseCasePalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input
+        System.out.print(" Input: ");
+        String input = scanner.nextLine();
+
+        // Convert string to character array
+        char[] characters = input.toCharArray();
+
+        // Two-pointer technique
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("Is Palindrome? : true");
+        } else {
+            System.out.println("Is Palindrome? : false");
+        }
+
+        scanner.close();    }
 }
+
